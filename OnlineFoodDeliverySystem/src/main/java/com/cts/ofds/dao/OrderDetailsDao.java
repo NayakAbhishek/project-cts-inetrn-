dfc004d40c6d6182909ae26f48ba9216f63110b2
Abhishek Nayak <65550205+NayakAbhishek@users.noreply.github.com>
2022-06-01 20:37:48 +0530

    Add files via upload

---
+++ OnlineFoodDeliverySystem/src/main/java/com/cts/ofds/dao/OrderDetailsDao.java
@@ -0,0 +1,23 @@
+package com.cts.ofds.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cts.ofds.model.OrderDetails;
+
+public interface OrderDetailsDao {
+
+	List<OrderDetails> list();
+	
+	Optional<OrderDetails> get(int id);
+	
+	int update(OrderDetails t,int id);
+	
+	int add(OrderDetails t);
+	
+	int delete(int id);
+	
+	int deleteAllOrder(int id);
+	
+	List<OrderDetails> getByUserId(String id);
+}
---
+++ OnlineFoodDeliverySystem/src/main/java/com/cts/ofds/model/OrderDetails.java
@@ -0,0 +1,71 @@
+package com.cts.ofds.model;
+
+public class OrderDetails {
+	private int orderId;
+	private String userId;
+	private String dishes;
+	private String orderDate;
+	private double price;
+	private String customerName;
+	
+	public OrderDetails() {
+	}
+	
+	public OrderDetails(String customerName, String userId, String dishes, String orderDate, double price) {
+		this.userId = userId;
+		this.dishes = dishes;
+		this.orderDate = orderDate;
+		this.price = price;
+		this.customerName = customerName;
+	}
+    
+	public String getcustomerName() {
+		return customerName;
+	}
+
+	public void setcustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getDishes() {
+		return dishes;
+	}
+	public void setDishes(String dishes) {
+		this.dishes = dishes;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderDetails [orderId=" + orderId + ", userId=" + userId + ", dishes=" + dishes + ", orderDate="
+				+ orderDate + ", price=" + price + ", customerName=" + customerName+"]";
+	}
+	
+	
+}
